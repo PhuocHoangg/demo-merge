f87e1865b1b1e45446a96dde1f9fc118bec0b4e9
PhuocHoangg <164506464+PhuocHoangg@users.noreply.github.com>
2025-11-12 22:12:56 +0700

    Demo

---
+++ src/main/java/org/example/demomerge/UserController.java
@@ -0,0 +1,20 @@
+package org.example.demomerge;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from main branch!";
+    }
+
+    @GetMapping("/user")
+    public User getUser() {
+        return new User(1L, "Alice");
+    }
+}
+
